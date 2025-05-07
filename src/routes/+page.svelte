faebef09bd687dfd409291bb9173279d5e96ff7e
W Santos <51366379+ahzzano@users.noreply.github.com>
2025-05-08 03:41:39 +0800

    feat: add dropdown buttons

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -23,9 +23,33 @@
 
 {#if $shops}
 	<div class="flex flex-col w">
-		<div class="flex w-full gap-2 p-2 bg-base-300">
-			<span class="w-2/3"> Shop </span>
-			<span class="w-1/3 sm:block hidden">City</span>
+		<div class="flex w-full gap-2 p-2 pl-4 pt-2 bg-base-300 justify-center items-center">
+			<div class="flex items-center w-2/3">
+				<span>Shops</span>
+				<div class="w-2/3">
+					<div class="dropdown dropdown-start ml-5">
+						<button tabindex="0" class="btn btn-soft">Add</button>
+						<div
+							tabindex="1"
+							class="menu dropdown-content bg-base-300 rounded-box z-1 mt-4 w-52 p-2 shadow-sm"
+						>
+							<AddShop />
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="items-center flex w-1/3 justify-center items-center gap-4 sm:block hidden">
+				<span class="">City</span>
+				<div class="dropdown dropdown-start dropdown-hover">
+					<button tabindex="0" class="btn btn-soft rounded-field">Add</button>
+					<div
+						tabindex="1"
+						class="menu dropdown-content bg-base-300 rounded-box z-1 mt-4 w-52 p-2 shadow-sm"
+					>
+						<AddCity />
+					</div>
+				</div>
+			</div>
 		</div>
 
 		{#each $shops as shop}
