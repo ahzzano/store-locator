b01fa547951e99e06a464b780c42dfa2e9577076
W Santos <51366379+ahzzano@users.noreply.github.com>
2025-05-07 23:02:27 +0800

    feat: atomize forms

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,18 +5,23 @@
 	import { liveQuery } from 'dexie';
 	import { db } from '../db';
 	import type { Shop } from '../types';
+	import ShopEntry from '../components/shopEntry.svelte';
+	import AddItem from '../components/addItem.svelte';
 
 	let shops = liveQuery(async () => {
 		const shops = await db.shops.toArray();
 		const resolvedShops = await Promise.all(
 			shops.map(async (shop) => {
 				const city = await db.cities.get(shop.city);
-				return { ...shop, city };
+				const fixedShop: Shop = { ...shop, city };
+				return fixedShop;
 			})
 		);
 
 		return resolvedShops;
 	});
+
+	let items = liveQuery(async () => db.items.toArray());
 
 	async function removeShop(shop: Shop) {
 		try {
@@ -27,23 +32,22 @@
 
 <AddCity />
 <AddShop />
+<AddItem />
 
 Shops
 {#if $shops}
 	<div class="flex flex-col w">
 		{#each $shops as shop}
-			<div class="flex w-full gap-2">
-				<span class="w-1/2">{shop.name}</span>
-				<span class="w-1/3">{shop.city.name}</span>
-				<button
-					class="w-1/10"
-					on:click={async () => {
-						await removeShop(shop);
-					}}>Remove</button
-				>
-			</div>
+			<ShopEntry {shop} />
 		{/each}
 	</div>
 {:else}
 	No Shops Available
+{/if}
+
+{#if $items}
+	{#each $items as item}
+		{item.name}
+		{item.shop}
+	{/each}
 {/if}
