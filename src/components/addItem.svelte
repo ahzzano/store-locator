acba08d203ef5d21f044ac5a908deb5434e794af
W Santos <51366379+ahzzano@users.noreply.github.com>
2025-05-07 23:02:11 +0800

    feat: add addItem form

--- src/components/addItem.svelte
+++ src/components/addItem.svelte
@@ -1,3 +1,34 @@
-<s-script lang="ts"></s-script>
+<script lang="ts">
+	import { db } from '../db';
+	import { liveQuery } from 'dexie';
 
-<form action=""></form>
+	let itemName = '';
+	let itemShop = 0;
+	let itemCategory = '';
+	let itemPrice = 0;
+
+	let shops = liveQuery(() => db.shops.toArray());
+
+	async function addItem() {
+		try {
+			await db.items.add({
+				name: itemName,
+				price: itemPrice,
+				shop: itemShop,
+				category: itemCategory
+			});
+		} catch (e) {}
+	}
+</script>
+
+<form on:submit={addItem}>
+	<input bind:value={itemName} class="input" placeholder="name" />
+	<input bind:value={itemCategory} class="input" placeholder="category" />
+	<input bind:value={itemPrice} type="number" class="input" placeholder="price" />
+	<select class="dropdown" bind:value={itemShop}>
+		{#each $shops as shop}
+			<option value={shop.id}>{shop.name}</option>
+		{/each}
+	</select>
+	<button type="submit">Add Item</button>
+</form>
