b58b9287c6dd31ac1826f7de34c762dee407ea8a
W Santos <51366379+ahzzano@users.noreply.github.com>
2025-05-07 21:24:03 +0800

    feat: add some scheme to db

--- src/db.ts
+++ src/db.ts
@@ -1,17 +1,17 @@
 // db.js
 import Dexie, { type EntityTable } from 'dexie';
-import { City, Shop, Item } from '@/types';
+import { type City, type Shop, type Item } from './types'
 
 export const db = new Dexie('Shops') as Dexie & {
-    cities: EntityTable<City>,
-    shops: EntityTable<Shop>
-    items: EntityTable<Item>
+    cities: EntityTable<City, 'id'>,
+    shops: EntityTable<Shop, 'id'>
+    items: EntityTable<Item, 'id'>
 
 };
 
 db.version(1).stores({
     cities: '++id, name',
-    shops: '++id, cityId',
+    shops: '++id, cityId, name',
     items: '++id, name, price, shopId',
 });
 
