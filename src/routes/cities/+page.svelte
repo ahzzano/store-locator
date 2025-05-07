a95e4a5696b1d7cf753a4b2b8e0b74f8336bce3b
W Santos <51366379+ahzzano@users.noreply.github.com>
2025-05-08 03:41:32 +0800

    feat: add cities page

--- src/routes/cities/+page.svelte
+++ src/routes/cities/+page.svelte
@@ -1,15 +1,30 @@
 <script lang="ts">
 	import { liveQuery } from 'dexie';
 	import { db } from '../../db';
+	import AddCity from '../../components/addCity.svelte';
 
 	let cities = liveQuery(async () => db.cities.toArray());
 </script>
 
-<div class="flex flex-col">
-	<div class="flex w-full gap-2 p-2 bg-base-300">
-		<span class="w-2/3"> City </span>
+<div class="flex flex-col w">
+	<div class="flex w-full gap-2 p-2 pl-4 bg-base-300 justify-center items-center">
+		<span>City</span>
+		<div class="w-2/3">
+			<div class="dropdown dropdown-start dropdown-hover">
+				<button tabindex="0" class="btn btn-ghost rounded-field">Add</button>
+				<div
+					tabindex="1"
+					class="menu dropdown-content bg-base-300 rounded-box z-1 mt-4 w-52 p-2 shadow-sm"
+				>
+					<AddCity />
+				</div>
+			</div>
+		</div>
 	</div>
-	{#each $cities as city}
-		<span>{city.name}</span>
-	{/each}
+
+	<div class="flex flex-col gap-5 justify-center sm:pl-5 sm:pr-5 mt-2">
+		{#each $cities as city}
+			<span>{city.name}</span>
+		{/each}
+	</div>
 </div>
