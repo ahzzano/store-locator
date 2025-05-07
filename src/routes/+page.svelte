1cd21996291f8a4d4cd93fac0db495840835282c
W Santos <51366379+ahzzano@users.noreply.github.com>
2025-05-07 21:23:54 +0800

    feat: add some forms to main page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,26 @@
 <script lang="ts">
+	import { liveQuery } from 'dexie';
+	import AddShop from '../components/addShop.svelte';
 	import { db } from '../db';
+
+	let cityName = '';
+	async function addCity() {
+		console.log(cityName);
+		try {
+			const city = await db.cities.add({ name: cityName });
+			console.log(city);
+		} catch {}
+	}
+
+	db.cities.clear();
 </script>
 
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
 
-<input />
+<form on:submit={addCity}>
+	<input type="text" bind:value={cityName} required />
+	<button type="submit" class="bg-amber-50"> Add City </button>
+</form>
+
+<AddShop />
