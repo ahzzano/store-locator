48c599c463dabefdbfd634898a24a0baf137d433
W Santos <51366379+ahzzano@users.noreply.github.com>
2025-05-08 02:02:12 +0800

    feat: add city page

---
+++ src/routes/cities/+page.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import { liveQuery } from 'dexie';
+	import { db } from '../../db';
+
+	let cities = liveQuery(async () => db.cities.toArray());
+</script>
+
+<div class="flex flex-col">
+	<div class="flex w-full gap-2 p-2 bg-base-300">
+		<span class="w-2/3"> City </span>
+	</div>
+	{#each $cities as city}
+		<span>{city.name}</span>
+	{/each}
+</div>
