0221ee9d6bd0ec8d5b9478e5830dba937803b5cc
W Santos <51366379+ahzzano@users.noreply.github.com>
2025-05-08 00:02:48 +0800

    feat: add book to db

--- src/db.ts
+++ src/db.ts
@@ -1,17 +1,18 @@
 // db.js
 import Dexie, { type EntityTable } from 'dexie';
-import { type City, type Shop, type Item } from './types'
+import { type City, type Shop, type Item, type Book } from './types'
 
 export const db = new Dexie('Shops') as Dexie & {
     cities: EntityTable<City, 'id'>,
     shops: EntityTable<Shop, 'id'>
     items: EntityTable<Item, 'id'>
-
+    books: EntityTable<Book, 'id'>
 };
 
 db.version(1).stores({
     cities: '++id, name',
     shops: '++id, cityId, name',
-    items: '++id, name, price, shopId',
+    items: '++id, name, shopId, price',
+    books: '++id, name, author'
 });
 
