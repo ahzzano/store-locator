282eef1be96007daf21868f1f727dc7cfedf19cf
W Santos <51366379+ahzzano@users.noreply.github.com>
2025-05-08 00:57:41 +0800

    feat: add everything

--- src/components/shopEntry.svelte
+++ src/components/shopEntry.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
 	import { liveQuery } from 'dexie';
 	import { db } from '../db';
-	import type { Shop } from '../types';
+	import type { Shop, Item } from '../types';
 	import Modal from './modal.svelte';
 	import AddItem from './addItem.svelte';
 
@@ -10,12 +10,26 @@
 	let selectedShop = $state(shop.id);
 	let showModal = $state(false);
 
+	let categories = liveQuery(async () => {
+		const res = db.items.orderBy('category').uniqueKeys();
+		return res;
+	});
+	let selectedCategory = $state('all');
+
 	let items = liveQuery(async () => {
-		const res = await db.items.toArray();
-		const filtered = res.filter((item) => {
-			return item.shop == shop.id;
-		});
-		return filtered;
+		if (selectedCategory == 'all') {
+			const res = await db.items.toArray();
+			const filtered = res.filter((item) => {
+				return item.shop == shop.id;
+			});
+			return filtered;
+		} else {
+			const res = await db.items.where('category').equalsIgnoreCase(selectedCategory).toArray();
+			const filtered = res.filter((item) => {
+				return item.shop == shop.id;
+			});
+			return filtered;
+		}
 	});
 
 	async function removeShop(shop: Shop) {
@@ -52,33 +66,64 @@
 
 <Modal bind:showModal>
 	{#snippet header()}
-		{shop.name}
+		<span>
+			{shop.name}
+		</span>
 	{/snippet}
 
 	Items
-	<div class="flex flex-col">
-		{#if $items}
-			<div class="flex flex-col">
-				{#each $items as item}
-					<div class="flex gap-2">
-						<span>{item.name}</span>
-						<span>{item.category}</span>
+	<div class="flex">
+		<div class="w-2/3">
+			<div class="flex gap-2">
+				{#if $items && $items.length > 0}
+					{#if $categories && $categories.length != 0}
 						<button
 							onclick={() => {
-								removeItem(item);
-							}}>Remove</button
+								selectedCategory = 'all';
+							}}>all</button
 						>
-					</div>
-				{/each}
+						{#each $categories as cat}
+							<button
+								onclick={() => {
+									selectedCategory = cat;
+								}}
+							>
+								{cat}
+							</button>
+						{/each}
+					{/if}
+				{/if}
 			</div>
-		{:else}
-			No items
-		{/if}
-
-		<AddItem itemShop={shop.id} />
-
-		<button onclick={() => (showModal = false)} class="">Close</button>
+			<div class="flex flex-col">
+				{#if $items && $items.length > 0}
+					<div class="flex flex-col">
+						{#each $items as item}
+							<div class="flex gap-2 w-full">
+								<span class="w-3/5">{item.name}</span>
+								<span class="w-1/5">{item.price}</span>
+								<button
+									onclick={() => {
+										removeItem(item);
+									}}
+									class="btn">Remove</button
+								>
+							</div>
+						{/each}
+					</div>
+				{:else}
+					<span class="w-full text-center pt-5"> No items </span>
+				{/if}
+			</div>
+		</div>
+		<div class="border-l-gray-500 border-1 ml-2 mr-4 border-dashed" />
+		<div class="w-1/3">
+			Add Item
+			<AddItem itemShop={shop.id} />
+		</div>
 	</div>
 
-	Books
+	<!-- Books -->
+	<div class="justify-center items-center w-full">
+		<button onclick={() => (showModal = false)} class="btn w-1/4">Close</button>
+	</div>
 </Modal>
