6b1ab234169d7b9c26b25a497121eef97daae64a
W Santos <51366379+ahzzano@users.noreply.github.com>
2025-05-08 00:03:19 +0800

    feat: add modal util

---
+++ src/components/modal.svelte
@@ -0,0 +1,19 @@
+<script>
+	let { showModal = $bindable(), header, children } = $props();
+	let dialog = $state();
+
+	$effect(() => {
+		if (showModal) {
+			dialog.showModal();
+		} else {
+			dialog.close();
+		}
+	});
+</script>
+
+<dialog bind:this={dialog} onclose={() => (showModal = false)} class="modal">
+	<div class="modal-box">
+		{@render header?.()}
+		{@render children?.()}
+	</div>
+</dialog>
